e447e41b4956e5ea49600f2fd0a40ef6ad23e30d
Icarogamer2441 <148513434+Icarogamer2441@users.noreply.github.com>
2024-07-16 09:34:10 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,10 @@
 
 python3 -m py_compile ./omasm.py
 python3 -m py_compile ./om.py
-mv __pycache__/om.cpython-312.pyc ./bin/om
-mv __pycache__/omasm.cpython-312.pyc ./bin/omasm
+python3 -m py_compile ./omang.py
+mv __pycache__/om.cpython-*.pyc ./bin/om
+mv __pycache__/omasm.cpython-*.pyc ./bin/omasm
+mv __pycache__/omang.cpython-*.pyc ./bin/omang
 chmod +x ./bin/om
 chmod +x ./bin/omasm
+chmod +x ./bin/omang
