7872d39711e1ec631d24dbdf1acb0c32783f0c99
Icarogamer2441 <148513434+Icarogamer2441@users.noreply.github.com>
2024-07-15 21:43:48 -0300

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python3 -m py_compile ./omasm.py
+python3 -m py_compile ./om.py
+mv __pycache__/om.cpython-312.pyc ./bin/om
+mv __pycache__/omasm.cpython-312.pyc ./bin/omasm
+chmod +x __pycache__/om
+chmod +x __pycache__/omasm
